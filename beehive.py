32c451576e133320c3ba680117d11790f65f9755
Shubhamkumar Patel <44108449+Shubh023@users.noreply.github.com>
2021-08-15 19:38:42 +0200

    Update beehive.py

--- beehive.py
+++ beehive.py
@@ -9,6 +9,8 @@ import matplotlib.pyplot as plt
 from skimage.color import rgb2gray
 from skimage.filters import meijering, sato, frangi, hessian, gaussian, median
 from skimage.morphology import disk, white_tophat, area_closing, skeletonize, thin, erosion, diamond, cube, ball
+
+
 # Please Enter the path to the folder in INPUT
 INPUT = "Data/TEST/"
 
